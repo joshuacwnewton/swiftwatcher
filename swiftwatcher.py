095d1767fc24a77b27c2de49197b8377316e999d
joshuacwnewton <joshuacwnewton@gmail.com>
2019-05-09 15:07:54 -0700

    [!] (cv2 bug) Refactoring process_video.py, Batch processing -> Queue system

--- swiftwatcher.py
+++ swiftwatcher.py
@@ -6,30 +6,18 @@ video_directory = "videos/gdrive_swiftvideos/"
 filename = "ch04_20170531210856.mp4"
 save_directory = video_directory + os.path.splitext(filename)[0]
 
-# Extraction configs
-start = "00:21:30:000"
-end = "00:23:30:000"
-batch_size = 1000
-
 # Create FrameStack object.
-frameStack = pv.FrameStack(video_directory, filename)
+frameStack = pv.FrameStack(video_directory, filename, 5)
 
-# Convert total timestamps into batch timestamps using provided configs
-batch_timestamps = frameStack.batch_config(start, end, batch_size)
-
-# Extract frames from video file and save them to a directory.
-print("[*] Requested time period: {} to {}.".format(start, end))
-print("[*] Batch mode: {} batches of size {} will be extracted.".format(len(batch_timestamps), batch_size))
-total_frames = 0
-for timestamp_pair in batch_timestamps:
-    frameStack.read_frames(start_timestamp=timestamp_pair[0], end_timestamp=timestamp_pair[1])
-    total_frames += len(frameStack.stack)
-
-    # TODO: Further preprocessing will obviously be required.
-    # Only basic steps had been written because of desire to test frameStack class behavior.
-    # frameStack.convert_grayscale()
-    frameStack.crop_frames_rect(corners=[(745, 617), (920, 692)])
-    frameStack.resize_frames(1000)
-    frameStack.save_frames(save_directory)
+print("[*] Reading frames... (This may take a while!)")
+while frameStack.frames_read < frameStack.src_framecount:
+    frameStack.read_frame(delay=1)
+    frameStack.convert_grayscale(0)
+    frameStack.crop_frame(0, corners=[(745, 617), (920, 692)])
+    frameStack.resize_frame(0, 1000)
+    frameStack.save_frame(0, save_directory)
+    if frameStack.frames_read % 1000 == 0:
+        print("{}/{} frames successfully processed.".format(frameStack.frames_read, frameStack.src_framecount))
+frameStack.stream.release()
 print("[========================================================]")
-print("[-] Extraction complete. {} total frames extracted.".format(total_frames))
+print("[-] Extraction complete. {} total frames extracted.".format(frameStack.frames_read))
--- swiftwatcher/process_video.py
+++ swiftwatcher/process_video.py
@@ -49,10 +49,11 @@ def timestamp_to_frameindex(timestamp, fps):
 class FrameStack:
     """Class for storing, describing, and manipulating a stack of frames from a video file.
 
-    Attributes: ssrc_filename, src_directory, src_fps, src_framecount
+    Attributes: src_filename, src_directory, src_fps, src_framecount, src_height, src_width
+                stack, indices, timestamps
     Methods: read_frames, save_frames, save_frames_info, load_frames, load_frames_info
     """
-    def __init__(self, video_directory, filename):
+    def __init__(self, video_directory, filename, stack_size):
         # Check validity of filepath
         video_filepath = video_directory + filename
         if not os.path.isfile(video_filepath):
@@ -61,139 +62,47 @@ class FrameStack:
         # Get attributes from source file that is associated with FrameStack.
         self.src_filename = filename
         self.src_directory = video_directory
-        stream = cv2.VideoCapture("{}/{}".format(self.src_directory, self.src_filename))
-        if not stream.isOpened():
+        self.stream = cv2.VideoCapture("{}/{}".format(self.src_directory, self.src_filename))
+        if not self.stream.isOpened():
             raise Exception("[!] Video file could not be opened to read frames. Check file path.")
         else:
-            self.src_fps = stream.get(cv2.CAP_PROP_FPS)
-            self.src_framecount = stream.get(cv2.CAP_PROP_FRAME_COUNT)
-        stream.release()
+            self.src_fps = self.stream.get(cv2.CAP_PROP_FPS)
+            self.src_framecount = self.stream.get(cv2.CAP_PROP_FRAME_COUNT)
+            self.src_height = int(self.stream.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            self.src_width = int(self.stream.get(cv2.CAP_PROP_FRAME_WIDTH))
+            self.src_codec = self.stream.get(cv2.CAP_PROP_FOURCC)
 
         # Initialize attributes of FrameStack
-        self.stack = []
-        self.indices = []
-        self.timestamps = []
-        self.start_index = None
-        self.end_index = None
+        self.stack = [None]*stack_size
+        self.indices = [None]*stack_size
+        self.timestamps = [' ']*stack_size
+        self.frames_read = 0
 
-    def batch_config(self, start_timestamp, end_timestamp, batch_size):
-        """Divides requested duration into batches, returns list of timestamp tuples.
-        Input arguments:
-            -start_timestamp: starting point for video duration to be extracted.
-            -end_timestamp: ending point for video duration to be extracted.
-            -batch_size: divides full duration into small batches of this size."""
+    def read_frame(self, delay=5):
+        """Stores a new frame into index 0 of frame stack."""
 
-        # Convert provided timestamps into frame indices
-        start_index = timestamp_to_frameindex(start_timestamp, self.src_fps)
-        end_index = timestamp_to_frameindex(end_timestamp, self.src_fps)
+        if not self.stream.isOpened():
+            raise Exception("[!] Video stream is not open. Cannot read new frames.")
 
-        batch_timestamps = []
-        while int(end_index - start_index) > batch_size:
-            # Calculate and store batch timestamps
-            batch_start_timestamp = frameindex_to_timestamp(start_index, self.src_fps)
-            batch_end_timestamp = frameindex_to_timestamp(start_index+batch_size-1, self.src_fps)
-            batch_timestamps.append((batch_start_timestamp, batch_end_timestamp))
-            
-            # Update timestamp pointer
-            start_index += batch_size
+        for i in range(delay):
+            success = self.stream.grab()
 
-        # Append remaining small batch (total_frames % batch_size, remainder is the small batch)
-        # (Also, returns input timestamps if duration is less than one batch.)
-        batch_start_timestamp = frameindex_to_timestamp(start_index, self.src_fps)
-        batch_end_timestamp = frameindex_to_timestamp(end_index, self.src_fps)
-        batch_timestamps.append((batch_start_timestamp, batch_end_timestamp))
+        # Shift frames in frame stack
+        np.roll(self.stack, 1, axis=0)
+        np.roll(self.indices, 1, axis=0)
+        np.roll(self.timestamps, 1, axis=0)
 
-        return batch_timestamps
+        # Fetch new frame and update its attributes
+        self.indices[0] = int(self.stream.get(cv2.CAP_PROP_POS_FRAMES))
+        self.timestamps[0] = (ms_to_timestamp(self.stream.get(cv2.CAP_PROP_POS_MSEC)))
+        success, frame = self.stream.retrieve()
+        self.stack[0] = np.array(frame)
+        if success:
+            self.frames_read += 1
 
-    def read_frames(self, start_timestamp='<Start of File>', end_timestamp='<End of File>',
-                    desired_fps='<Source FPS>', verbose=False):
-        """Saves a set of frames into the stack attribute of the class object.
+        return success
 
-        Input arguments:
-            -start_timestamp: starting point for video duration to be extracted.
-            -end_timestamp: ending point for video duration to be extracted.
-            -desired_fps: allows for subsampling at rates lower than the source FPS.
-            -verbose: controls status updates to the console.
-
-        Updated attributes:
-            -self.stack: list of RGB frames from specified video duration.
-            -self.indices: list of absolute indices corresponding to the original video.
-            -self.timestamps: timestamps formatted as 'HH:MM:SS:MSS'."""
-
-        # Attempt to open video capture object
-        stream = cv2.VideoCapture("{}/{}".format(self.src_directory, self.src_filename))
-        if not stream.isOpened():
-            raise Exception("[!] Video file could not be opened to read frames. Check file path.")
-        
-        # Set start index using either provided value or end-of-file.
-        if start_timestamp == '<Start of File>':
-            self.start_index = 0
-        else:
-            self.start_index = timestamp_to_frameindex(start_timestamp, self.src_fps)
-            if not 0 <= self.start_index < self.src_framecount:
-                raise Exception("[!] Invalid start timestamp. Outside range of acceptable times.")
-
-        # Set end index using either provided value or end-of-file.
-        if end_timestamp == '<End of File>':
-            self.end_index = self.src_framecount-1
-        else:
-            self.end_index = timestamp_to_frameindex(end_timestamp, self.src_fps)
-            if not self.start_index < self.end_index < self.src_framecount:
-                raise Exception("[!] Invalid end timestamp. Outside range of acceptable times.")
-
-        # Set desired fps if not provided.
-        if desired_fps == '<Source FPS>':
-            desired_fps = self.src_fps
-
-        # Flush any previously read frame information
-        self.stack = []
-        self.indices = []
-        self.timestamps = []
-
-        # Initialize frame counter and frame index.
-        frame_index = self.start_index
-        stream.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
-        frame_count = 0
-
-        print("[========================================================]")
-        print("[*] Extracting frames from {} to {}.".format(start_timestamp, end_timestamp))
-        while stream.isOpened():
-            # Fetch frame
-            success, frame = stream.read()
-
-            if not success:
-                print("[*] No frame left to be read. Exiting read_frames().")
-                stream.release()
-                break
-
-            # Get attributes of frame.
-            self.stack.append(frame)
-            self.indices.append(frame_index)
-            self.timestamps.append(ms_to_timestamp(stream.get(cv2.CAP_PROP_POS_MSEC)))
-
-            # Increment frame counter and update frame index.
-            frame_count += 1
-            if desired_fps == self.src_fps:
-                frame_index += 1
-            elif 0 < desired_fps < self.src_fps:
-                frame_index += round(self.src_fps/desired_fps)
-                stream.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
-            else:
-                raise Exception("[!] Invalid FPS. Must be positive and not greater than source file FPS.")
-            
-            # Provide status updates
-            if verbose and frame_count % 100 == 0:
-                print("[-] Progress update: {} frames processed.".format(frame_count))
-
-            # Check end condition
-            if stream.get(cv2.CAP_PROP_POS_FRAMES) > timestamp_to_frameindex(end_timestamp, self.src_fps):
-                print("[-] Specified ending time reached. Exiting read_frames().")
-                stream.release()
-                break
-
-        print("[-] Frame extraction completed. {} frames extracted.".format(frame_count))
-
-    def save_frames(self, save_directory):
+    def save_frame(self, index, save_directory):
         # TODO: Write a proper docstring for the save_frames() method.
         """Saves a set of frames to a """
         # Check for directory existance
@@ -206,46 +115,37 @@ class FrameStack:
             else:
                 print("[-] Successfully created the directory %s." % save_directory)
 
-        # Save frames to specified directory
-        print("[*] Saving frames...")
-        count = 0
-        for index in range(len(self.stack)):
+        time = self.timestamps[index].split(":")
+        if not os.path.isdir(save_directory+"/"+time[0]+":"+time[1]):
             try:
-                cv2.imwrite("{0}/frame{1}_{2}.jpg".format(save_directory,
-                                                          self.indices[index],
-                                                          self.timestamps[index]),
-                            self.stack[index])
-            except Exception as e:
-                print("[!] Image saving failed due to: {0}".format(str(e)))
-            count += 1
+                os.mkdir(save_directory+"/"+time[0]+":"+time[1])
+            except OSError:
+                print("[!] Creation of the directory %s failed." % save_directory+"/"+time[0]+":"+time[1])
 
-        print("[-] {} frames successfully saved.".format(count))
-
-    def convert_grayscale(self):
-        # TODO: Write a proper docstring for the convert_grayscale() method.
-        print("[*] Converting frames to grayscale...")
         try:
-            self.stack = np.array([cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
-                                   for color_image in self.stack])
-            print("[-] Frame conversion successfully completed.")
+            cv2.imwrite("{0}/frame{2}_{1}.jpg".format(save_directory+"/"+time[0]+":"+time[1],
+                                                      self.indices[index],
+                                                      self.timestamps[index]),
+                        self.stack[index])
+        except Exception as e:
+            print("[!] Image saving failed due to: {0}".format(str(e)))
+
+    def convert_grayscale(self, index):
+        # TODO: Write a proper docstring for the convert_grayscale() method.
+        try:
+            self.stack[index] = cv2.cvtColor(self.stack[index], cv2.COLOR_BGR2GRAY)
         except Exception as e:
             print("[!] Frame conversion failed due to: {0}".format(str(e)))
 
-    def crop_frames_rect(self, corners):
+    def crop_frame(self, index, corners):
         # TODO: Write a proper docstring for the crop_frames_rect() method.
-        height = corners[1][1] - corners[0][1]
-        width = corners[1][0] - corners[0][0]
-        print("[*] Cropping frames to size {}x{}...".format(height, width))
         try:
-            self.stack = np.array([image[corners[0][1]:corners[1][1], corners[0][0]:corners[1][0]]
-                                  for image in self.stack])
-            print("[-] Frame cropping successfully completed.")
+            self.stack[index] = self.stack[index][corners[0][1]:corners[1][1], corners[0][0]:corners[1][0]]
         except Exception as e:
             print("[!] Frame cropping failed due to: {0}".format(str(e)))
 
-    def resize_frames(self, scale_percent):
+    def resize_frame(self, index, scale_percent):
         s = scale_percent/100
-        self.stack = np.array([cv2.resize(frame,
-                                          (round(frame.shape[1]*s), round(frame.shape[0]*s)),
-                                          interpolation=cv2.INTER_AREA)
-                               for frame in self.stack])
+        self.stack[index] = cv2.resize(self.stack[index],
+                                       (round(self.stack[index].shape[1]*s), round(self.stack[index].shape[0]*s)),
+                                       interpolation=cv2.INTER_AREA)
