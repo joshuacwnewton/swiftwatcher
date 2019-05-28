649b85917c18c45afe486f9f61ae386ef5eddf44
joshuacwnewton <joshuacwnewton@gmail.com>
2019-05-28 14:33:27 -0700

    Refactoring code to be in line with PEP8 (line length guidelines).

--- swiftwatcher.py
+++ swiftwatcher.py
@@ -6,29 +6,34 @@ import cv2
 import numpy as np
 from scipy import ndimage as img
 
+# TODO: Rewrite to comply with line length
+
 
 def main(args):
     # Code to extract all frames from video and save them to files
     if args.extract:
         pv.extract_frames(args.video_dir, args.filename)
 
-    # Code to load previously extracted frames and apply image processing to them
+    # Code to process previously extracted frames
     if args.reuse:
         # Initialize parameters necessary to load previously extracted frames
-        load_directory = (args.video_dir +  # Assumed convention from pv.extract_frames()
+        # Directory follows assumed convention from pv.extract_frames()
+        load_directory = (args.video_dir +
                           os.path.splitext(args.filename)[0])
-        load_index = args.reuse[0]          # Index of frame to load next
-        total_frames = args.reuse[1]        # Total number of frames to load and process
+        load_index = args.reuse[0]
+        total_frames = args.reuse[1]
 
         # Create FrameQueue object
         queue_size = 20
         queue_center = int((queue_size-1)/2)
-        frame_queue = pv.FrameQueue(args.video_dir, args.filename, queue_size=queue_size)
-        frame_queue.stream.release()  # VideoCapture not needed if frames are being reused
+        frame_queue = pv.FrameQueue(args.video_dir, args.filename,
+                                    queue_size=queue_size)
+        frame_queue.stream.release()  # Not needed for frame reuse
 
         while frame_queue.frames_read < total_frames:
             # Load frame with specified index into FrameQueue object
-            success = frame_queue.load_frame_from_file(load_directory, load_index)
+            success = frame_queue.load_frame_from_file(load_directory,
+                                                       load_index)
 
             # Process frame (grayscale, segmentation, etc.)
             if success:
@@ -38,53 +43,72 @@ def main(args):
                 frame_queue.frame_to_column()
 
                 if frame_queue.frames_read > queue_center:
-                    # Robust PCA using adjacent frames from forward and backwards in time
-                    lowrank, sparse = frame_queue.rpca_decomposition(index=queue_center,
-                                                                     darker_only=True)
+                    # Choosing index such that RPCA will use adjacent frames
+                    # (forward and backwards) to "queue_center" frame
+                    lowrank, sparse = \
+                        frame_queue.rpca_decomposition(index=queue_center,
+                                                       darker_only=True)
 
                     # Apply bilateral filter to remove noise, retain birds
                     sparse_filtered = sparse
                     for i in range(2):
-                        sparse_filtered = cv2.bilateralFilter(sparse_filtered, 7, 15, 1)
+                        sparse_filtered = cv2.bilateralFilter(sparse_filtered,
+                                                              d=7,
+                                                              sigmaColor=15,
+                                                              sigmaSpace=1)
 
                     # Apply grayscale opening
-                    #for i in range(5):
-                    #     sparse_opened = img.grey_opening(sparse_filtered, size=(2, 2))
-                    # sparse_eroded = img.grey_erosion(sparse_opened, size=(1, 1))
+                    # for i in range(5):
+                    #     sparse_opened = img.grey_opening(sparse_filtered,
+                    #                                      size=(2, 2))
+                    # sparse_eroded = img.grey_erosion(sparse_opened,
+                    #                                  size=(1, 1))
 
-                    ret, sparse_thresh = cv2.threshold(sparse_filtered, 35, 255, cv2.THRESH_TOZERO)
-                    sparse_opened = img.binary_opening(sparse_thresh,
-                                                       structure=np.ones((2, 2))).astype(sparse_thresh.dtype)
-                    # Threshold result of bilateral filter, backproject onto motion estimated image
-                    # _, mask = cv2.threshold(sparse_filtered, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+                    ret, sparse_thr = cv2.threshold(sparse_filtered,
+                                                    thresh=35,
+                                                    maxval=255,
+                                                    type=cv2.THRESH_TOZERO)
+                    element = np.ones((2, 2))
+                    sparse_opened = \
+                        img.binary_opening(sparse_thr, structure=element) \
+                        .astype(sparse_thr.dtype)
+
+                    # Threshold result of bilateral filter, then backproject
+                    # onto motion estimated image.
+                    # _, mask = cv2.threshold(sparse_filtered,
+                    #                         thresh=0,
+                    #                         maxval=255,
+                    #                         type=(cv2.THRESH_BINARY+
+                    #                               cv2.THRESH_OTSU))
                     # mask[mask == 255] = 1
                     # sparse_thresh = np.multiply(sparse_uint8, mask)
 
-                    retval, sparse_cc = cv2.connectedComponents(sparse_opened, connectivity=4)
+                    # TODO: Clarify "ret" and "retval" names
+                    retval, sparse_cc = cv2.connectedComponents(sparse_opened,
+                                                                connectivity=4)
                     if retval:
                         sparse_cc = sparse_cc*(255/retval)
 
-                    # Print 3 stages of filtering to compare
-                    frame = np.reshape(frame_queue.queue[queue_center], (frame_queue.height, frame_queue.width))
-                    sparse_frames = np.vstack((frame, sparse_thresh, sparse_cc))
-                    frame_queue.save_frame_to_file(load_directory, frame=sparse_frames,
+                    # Reshape column vector form of image into normal frame
+                    frame = np.reshape(frame_queue.queue[queue_center],
+                                       (frame_queue.height, frame_queue.width))
+                    separator = 255*np.ones(shape=(1, frame_queue.width),
+                                            dtype=np.uint8)
+                    sparse_frames = np.vstack((frame, separator,
+                                               sparse, separator,
+                                               sparse_filtered, separator,
+                                               sparse_thr, separator,
+                                               sparse_cc))
+                    frame_queue.save_frame_to_file(load_directory,
+                                                   frame=sparse_frames,
                                                    index=queue_center,
                                                    folder_name=args.custom_dir,
                                                    scale=400)
-                    # Unused elements of RPCA
-                    # frame_queue.save_frame_to_file(load_directory, frame=lowrank,
-                    #                                index=queue_center,
-                    #                                folder_name=args.custom_dir,
-                    #                                file_suffix="b_RPCAlowrank", scale=400)
-                    # sparse_cm = cm.apply_custom_colormap(sparse_abs, cmap="viridis")
-                    # frame_queue.save_frame_to_file(load_directory, frame=sparse_cm,
-                    #                                index=queue_center,
-                    #                                folder_name=args.custom_dir,
-                    #                                file_suffix="c_RPCAsparsecm", scale=400)
 
             load_index += (1 + frame_queue.delay)
             if frame_queue.frames_read % 50 == 0:
-                print("{0}/{1} frames processed.".format(frame_queue.frames_read, total_frames))
+                print("{0}/{1} frames processed."
+                      .format(frame_queue.frames_read, total_frames))
 
 
 if __name__ == "__main__":
@@ -114,7 +138,7 @@ if __name__ == "__main__":
                         )
     parser.add_argument("-c",
                         "--custom_dir",
-                        help="Custom directory for extracted frame image files",
+                        help="Custom directory for extracted frame files",
                         default="Argument testing"
                         )
     parser.add_argument("-p",
--- swiftwatcher/process_video.py
+++ swiftwatcher/process_video.py
@@ -8,20 +8,26 @@ from utils.rpca_ialm import inexact_augmented_lagrange_multiplier
 
 
 class FrameQueue:
-    """Class for storing, describing, and manipulating frames from video file using a FIFO queue.
-    Essentially a deque with additional attributes and methods specific to video analysis.
-    Queue is size 1 by default. Increase queue size for analysis requiring multiple sequential frames.
+    """Class for storing, describing, and manipulating frames from video file
+    using a FIFO queue. Essentially a deque with additional attributes and
+    methods specific to video analysis.
+
+    Queue is size 1 by default. Increase queue size for analysis requiring
+    multiple sequential frames.
 
     Attributes (frame queue):  queue, framenumbers, timestamps, frames_read
-    Attributes (source video): src_filename, src_directory, src_fps, src_framecount,
-                               src_height, src_width, src_codec
-    Attributes (frames):       height, width, delay, fps
+    Attributes (source video): src_filename, src_directory, src_fps,
+                               src_framecount, src_height, src_width, src_codec
+    Attributes (frames): height, width, delay, fps
 
-    Methods (File I/O): load_frame_from_video, load_frame_from_file, save_frame_to_file,
-    Methods (Frame processing): convert_grayscale, segment_frame, crop_frame, resize_frame,
-                                frame_to_column, rpca_decomposition
+    Methods (File I/O): load_frame_from_video, load_frame_from_file,
+                        save_frame_to_file,
+    Methods (Frame processing): convert_grayscale, segment_frame, crop_frame,
+                                resize_frame, frame_to_column,
+                                rpca_decomposition
     """
-    def __init__(self, video_directory, filename, queue_size=1, desired_fps=False):
+    def __init__(self, video_directory, filename, queue_size=1,
+                 desired_fps=False):
         # Initialize queue attributes
         self.queue = collections.deque([], queue_size)
         self.framenumbers = collections.deque([], queue_size)
@@ -36,33 +42,39 @@ class FrameQueue:
         # Open source video file and store its immutable attributes
         self.src_filename = filename
         self.src_directory = video_directory
-        self.stream = cv2.VideoCapture("{}/{}".format(self.src_directory, self.src_filename))
+        self.stream = cv2.VideoCapture("{}/{}".format(self.src_directory,
+                                                      self.src_filename))
         if not self.stream.isOpened():
-            raise Exception("[!] Video file could not be opened to read frames. Check file path.")
+            raise Exception("[!] Video file could not be opened to read"
+                            " frames. Check file path.")
         else:
             self.src_fps = self.stream.get(cv2.CAP_PROP_FPS)
-            self.src_framecount = int(self.stream.get(cv2.CAP_PROP_FRAME_COUNT))
+            self.src_framecount = int(self.stream.get
+                                      (cv2.CAP_PROP_FRAME_COUNT))
             self.src_height = int(self.stream.get(cv2.CAP_PROP_FRAME_HEIGHT))
             self.src_width = int(self.stream.get(cv2.CAP_PROP_FRAME_WIDTH))
 
         # Initialize mutable frame/video attributes
-        self.height = self.src_height  # Separate because cropping may change dimensions
-        self.width = self.src_width    # Separate because cropping may change dimensions
+        self.height = self.src_height  # Separate b/c dimensions may change
+        self.width = self.src_width    # Separate b/c dimensions may change
         if not desired_fps:
             self.fps = self.src_fps
-            self.delay = round(self.src_fps / self.fps) - 1  # Needed to subsample video
+            self.delay = round(self.src_fps / self.fps) - 1
         else:
             self.fps = desired_fps
-            self.delay = 0
+            self.delay = 0  # Delay parameter needed to subsample video
 
     def load_frame_from_video(self):
-        """Insert next frame from video stream into left side (index 0) of frame queue."""
+        """Insert next frame from stream into left side (index 0) of queue."""
         if not self.stream.isOpened():
-            raise Exception("[!] Video stream is not open. Cannot read new frames.")
+            raise Exception("[!] Video stream is not open."
+                            " Cannot read new frames.")
 
         # Fetch new frame and update its attributes
-        self.framenumbers.appendleft(int(self.stream.get(cv2.CAP_PROP_POS_FRAMES)))
-        self.timestamps.appendleft((ms_to_timestamp(self.stream.get(cv2.CAP_PROP_POS_MSEC))))
+        self.framenumbers.appendleft(
+            int(self.stream.get(cv2.CAP_PROP_POS_FRAMES)))
+        self.timestamps.appendleft(
+            (ms_to_timestamp(self.stream.get(cv2.CAP_PROP_POS_MSEC))))
         success, frame = self.stream.read()
         if success:
             self.queue.appendleft(np.array(frame))
@@ -74,8 +86,9 @@ class FrameQueue:
 
         return success
 
-    def load_frame_from_file(self, base_save_directory, frame_number, folder_name=None):
-        """Insert specified frame from file into left side (index 0) of frame queue."""
+    def load_frame_from_file(self, base_save_directory, frame_number,
+                             folder_name=None):
+        """Insert frame from file into left side (index 0) of queue."""
         # Update frame attributes
         self.framenumbers.appendleft(frame_number)
         timestamp = framenumber_to_timestamp(frame_number, self.fps)
@@ -89,7 +102,9 @@ class FrameQueue:
             save_directory = base_save_directory+"/"+folder_name
 
         # Load frame from file
-        file_paths = glob.glob("{}/frame{}_{}*".format(save_directory, frame_number, timestamp))
+        file_paths = glob.glob("{0}/frame{1}_{2}*".format(save_directory,
+                                                          frame_number,
+                                                          timestamp))
         frame = cv2.imread(file_paths[0])
         self.queue.appendleft(np.array(frame))
 
@@ -104,11 +119,11 @@ class FrameQueue:
     def save_frame_to_file(self, base_save_directory, frame=None, index=0,
                            folder_name=None, file_prefix="", file_suffix="",
                            scale=100):
-        """Save an individual frame to an image file. If frame itself is not provided,
-           Frame will be pulled from frame_queue at specified index."""
+        """Save an individual frame to an image file. If frame itself is not
+        provided, frame will be pulled from frame_queue at specified index."""
 
-        # By default, frames will be saved in a subfolder corresponding to HH:MM.
-        # If specified, however, a custom subfolder can be chosen instead.
+        # By default, frames will be saved in a subfolder corresponding to
+        # HH:MM formatting. However, a custom subfolder can be chosen instead.
         if not folder_name:
             time = self.timestamps[index].split(":")
             save_directory = base_save_directory+"/"+time[0]+":"+time[1]
@@ -119,43 +134,47 @@ class FrameQueue:
         if not os.path.isdir(save_directory):
             try:
                 os.makedirs(save_directory)
-                sleep(0.5)  # Sometimes frame 0 won't be saved if delay is omitted
+                sleep(0.5)  # Sometimes frame 0 won't be saved without a delay
             except OSError:
-                print("[!] Creation of the directory %s failed." % save_directory)
+                print("[!] Creation of the directory {0} failed."
+                      .format(save_directory))
 
         # Extract frame from specified queue object as fallback
         if frame is None:
             frame = self.queue[index]
 
         # Resize frame for viewing convenience
-        s = scale/100
-        if s is not 1:
-            resized_frame = cv2.resize(frame,
-                                       (round(frame.shape[1]*s), round(frame.shape[0]*s)),
-                                       interpolation=cv2.INTER_AREA)
+        if scale is not 100:
+            s = scale / 100
+            frame = cv2.resize(frame,
+                               (round(frame.shape[1]*s),
+                                round(frame.shape[0]*s)),
+                               interpolation=cv2.INTER_AREA)
 
         # Write frame to image file within save_directory
         try:
-            cv2.imwrite("{0}/{1}frame{2}_{3}{4}.jpg".format(save_directory,
-                                                            file_prefix,
-                                                            self.framenumbers[index],
-                                                            self.timestamps[index],
-                                                            file_suffix),
-                        resized_frame)
+            cv2.imwrite("{0}/{1}frame{2}_{3}{4}.jpg"
+                        .format(save_directory, file_prefix,
+                                self.framenumbers[index],
+                                self.timestamps[index],
+                                file_suffix),
+                        frame)
         except Exception as e:
             print("[!] Image saving failed due to: {0}".format(str(e)))
 
     def convert_grayscale(self, index=0):
         """Convert to grayscale a frame at specified index of FrameQueue"""
         try:
-            self.queue[index] = cv2.cvtColor(self.queue[index], cv2.COLOR_BGR2GRAY)
+            self.queue[index] = cv2.cvtColor(self.queue[index],
+                                             cv2.COLOR_BGR2GRAY)
         except Exception as e:
             print("[!] Frame conversion failed due to: {0}".format(str(e)))
 
     def crop_frame(self, corners, index=0):
         """Crop frame at specified index of FrameQueue."""
         try:
-            self.queue[index] = self.queue[index][corners[0][1]:corners[1][1], corners[0][0]:corners[1][0]]
+            self.queue[index] = self.queue[index][corners[0][1]:corners[1][1],
+                                                  corners[0][0]:corners[1][0]]
         except Exception as e:
             print("[!] Frame cropping failed due to: {0}".format(str(e)))
 
@@ -170,7 +189,8 @@ class FrameQueue:
     def resize_frame(self, scale_percent, index=0):
         s = scale_percent/100
         self.queue[index] = cv2.resize(self.queue[index],
-                                       (round(self.queue[index].shape[1]*s), round(self.queue[index].shape[0]*s)),
+                                       (round(self.queue[index].shape[1]*s),
+                                        round(self.queue[index].shape[0]*s)),
                                        interpolation=cv2.INTER_AREA)
 
     def frame_to_column(self, index=0):
@@ -179,22 +199,25 @@ class FrameQueue:
                                                   (self.width*self.height, 1)))
 
     def rpca_decomposition(self, index, darker_only=False):
-        """Decompose set of images into corresponding low-rank and sparse images.
-        Method expects images to have been reshaped to matrix of column vectors.
+        """Decompose set of images into corresponding low-rank and sparse
+        images. Method expects images to have been reshaped to matrix of
+        column vectors.
 
-        NOTE: Currently this code inputs a batch of frames (the entire queue) and outputs
-        the decomposition only for a specific frame. This ensures the best accuracy (uses
-        context from before and after specified frame) but is inefficient.
-        Could be tweaked in the future."""
+        NOTE: Currently this code inputs a batch of frames (the entire queue)
+        and outputs the decomposition only for a specific frame. This ensures
+        the best accuracy (uses context from before and after specified frame)
+        but is inefficient. This could be tweaked in the future to improve
+        efficiency."""
 
-        # np.array alone would give an e.g. 20x12500x1 matrix. Adding
-        # np.transpose and np.squeeze yields 12500x20 (i.e. matrix of column vectors)
+        # np.array alone would give an e.g. 20x12500x1 matrix. Adding transpose
+        # and squeeze yields 12500x20. (i.e. a matrix of column vectors)
         matrix = np.squeeze(np.transpose(np.array(self.queue)))
 
         # Algorithm for the IALM approximation of Robust PCA method.
-        lr_columns, s_columns = inexact_augmented_lagrange_multiplier(matrix, verbose=False)
+        lr_columns, s_columns = \
+            inexact_augmented_lagrange_multiplier(matrix, verbose=False)
 
-        # Slice key frame from low rank, sparse results and reshape back into image
+        # Slice frame from low rank and sparse and reshape back into image
         lr_image = np.reshape(lr_columns[:, index], (self.height, self.width))
         s_image = np.reshape(s_columns[:, index], (self.height, self.width))
 
@@ -207,10 +230,11 @@ class FrameQueue:
 
 
 def ms_to_timestamp(total_ms):
-    """Helper function to convert millisecond value into formatted timestamp."""
-    # cv2's VideoCapture class provides the position of the video in milliseconds
-    # (cv2.CAP_PROP_POS_MSEC). However, as it is easier to think of video in terms of
-    # hours, minutes, and seconds, it's helpful to convert to a formatted timestamp.
+    """Helper function to convert millisecond value into timestamp."""
+    # cv2's VideoCapture class provides the position of the video in
+    # milliseconds (cv2.CAP_PROP_POS_MSEC). However, as it can be easier to
+    # think of video in terms of hours, minutes, and seconds, it's helpful to
+    # be able to convert back and forth.
     total_s = int(total_ms/1000)
     total_m = int(total_s/60)
     total_h = int(total_m/60)
@@ -225,10 +249,11 @@ def ms_to_timestamp(total_ms):
 
 
 def framenumber_to_timestamp(frame_number, fps):
-    """Helper function to convert an amount of frames into a formatted timestamp."""
-    # cv2's VideoCapture class provides a frame count property (cv2.CAP_PROP_FRAME_COUNT)
-    # but not a duration property. However, as it is easier to think of video in terms of
-    # duration/timestamps, it's helpful to convert back and forth.
+    """Helper function to convert an amount of frames into a timestamp."""
+    # cv2's VideoCapture class provides a frame count property
+    # (cv2.CAP_PROP_FRAME_COUNT) but not a duration property. However, as it
+    # can be easier to think of video in terms of hours, minutes, and seconds,
+    # it's helpful to be able to convert back and forth.
     milliseconds = (frame_number / fps)*1000
     timestamp = ms_to_timestamp(milliseconds)
 
@@ -236,10 +261,11 @@ def framenumber_to_timestamp(frame_number, fps):
 
 
 def timestamp_to_framenumber(timestamp, fps):
-    """Helper function to convert formatted timestamp into an amount of frames."""
-    # cv2's VideoCapture class provides a frame count property (cv2.CAP_PROP_FRAME_COUNT)
-    # but not a duration property. However, as it is easier to think of video in terms of
-    # duration/timestamps, it's helpful to convert back and forth.
+    """Helper function to convert timestamp into an amount of frames."""
+    # cv2's VideoCapture class provides a frame count property
+    # (cv2.CAP_PROP_FRAME_COUNT) but not a duration property. However, as it
+    # can be easier to think of video in terms of hours, minutes, and seconds,
+    # it's helpful to be able to convert back and forth.
     time = timestamp.split(":")
     seconds = (float(time[0])*60*60 +
                float(time[1])*60 +
@@ -249,7 +275,8 @@ def timestamp_to_framenumber(timestamp, fps):
     return frame_number
 
 
-def extract_frames(video_directory, filename, queue_size=1, save_directory=None):
+def extract_frames(video_directory, filename, queue_size=1,
+                   save_directory=None):
     """Helper function to extract individual frames one at a time 
        from video file and save them to image files for future reuse."""
 
@@ -273,9 +300,10 @@ def extract_frames(video_directory, filename, queue_size=1, save_directory=None)
 
         # Status updates
         if frame_queue.frames_read % 1000 == 0:
-            print("[-] {}/{} frames successfully processed.".format(frame_queue.frames_read,
-                                                                    frame_queue.src_framecount))
+            print("[-] {}/{} frames successfully processed."
+                  .format(frame_queue.frames_read, frame_queue.src_framecount))
     frame_queue.stream.release()
 
     print("[========================================================]")
-    print("[-] Extraction complete. {} total frames extracted.".format(frame_queue.frames_read))
+    print("[-] Extraction complete. {} total frames extracted."
+          .format(frame_queue.frames_read))
