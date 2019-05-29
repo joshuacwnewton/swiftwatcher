2e35c3cfead723bc96419a8d67028709dd0d4c3e
joshuacwnewton <joshuacwnewton@gmail.com>
2019-05-29 10:41:14 -0700

    Test config, total bird count: GS, RPCA, BL, THR, CC (no GS morph)

--- swiftwatcher.py
+++ swiftwatcher.py
@@ -1,9 +1,10 @@
 import swiftwatcher.process_video as pv
 import os
 import argparse as ap
-import utils.cm as cm
 import cv2
 import numpy as np
+
+import utils.cm as cm
 from scipy import ndimage as img
 
 
@@ -28,6 +29,11 @@ def main(args):
                                     queue_size=queue_size)
         frame_queue.stream.release()  # Not needed for frame reuse
 
+        # Initialize data structures for bird counting stats
+        bird_count = np.array([])
+        ground_truth = np.genfromtxt('videos/groundtruth.csv',
+                                     delimiter=',').astype(dtype=int)
+
         while frame_queue.frames_read < total_frames:
             # Load frame with specified index into FrameQueue object
             success = frame_queue.load_frame_from_file(load_directory,
@@ -35,7 +41,7 @@ def main(args):
 
             # Process frame (grayscale, segmentation, etc.)
             if success:
-                # Preprocessing
+                # Processing steps prior to motion estimation
                 frame_queue.convert_grayscale()
                 frame_queue.crop_frame(corners=args.crop)
                 frame_queue.frame_to_column()
@@ -47,7 +53,7 @@ def main(args):
                         frame_queue.rpca_decomposition(index=queue_center,
                                                        darker_only=True)
 
-                    # Apply bilateral filter to remove noise, retain birds
+                    # Apply bilateral filter to remove artifacts, retain birds
                     sparse_filtered = sparse
                     for i in range(2):
                         sparse_filtered = cv2.bilateralFilter(sparse_filtered,
@@ -55,42 +61,29 @@ def main(args):
                                                               sigmaColor=15,
                                                               sigmaSpace=1)
 
-                    # Apply grayscale opening
-                    # for i in range(5):
-                    #     sparse_opened = img.grey_opening(sparse_filtered,
-                    #                                      size=(2, 2))
-                    # sparse_eroded = img.grey_erosion(sparse_opened,
-                    #                                  size=(1, 1))
-
+                    # Retain strongest areas and discard the rest
                     _, sparse_thr = cv2.threshold(sparse_filtered,
                                                   thresh=35,
                                                   maxval=255,
                                                   type=cv2.THRESH_TOZERO)
-                    element = np.ones((2, 2))
-                    sparse_opened = \
-                        img.binary_opening(sparse_thr, structure=element) \
-                        .astype(sparse_thr.dtype)
 
-                    # Threshold result of bilateral filter, then backproject
-                    # onto motion estimated image.
-                    # _, mask = cv2.threshold(sparse_filtered,
-                    #                         thresh=0,
-                    #                         maxval=255,
-                    #                         type=(cv2.THRESH_BINARY+
-                    #                               cv2.THRESH_OTSU))
-                    # mask[mask == 255] = 1
-                    # sparse_thresh = np.multiply(sparse_uint8, mask)
+                    # Segment using connected component labeling
+                    retval, sparse_cc = \
+                        cv2.connectedComponents(sparse_thr, connectivity=4)
+                    # Scale CC image for visual clarity
+                    if retval > 0:
+                        sparse_cc = sparse_cc*(255/retval)
 
-                    num_components, sparse_cc = \
-                        cv2.connectedComponents(sparse_opened, connectivity=4)
-                    if num_components > 0:
-                        sparse_cc = sparse_cc*(255/num_components)
+                    # Exclude background, count only foreground segments
+                    num_components = retval - 1
+                    if 16199 < (load_index - queue_center) < 16391:
+                        bird_count = np.append(bird_count, num_components)
 
-                    # Reshape column vector form of image into normal frame
+                    # Display different stages of processing in single image
+                    separator = 255 * np.ones(shape=(1, frame_queue.width),
+                                              dtype=np.uint8)
                     frame = np.reshape(frame_queue.queue[queue_center],
                                        (frame_queue.height, frame_queue.width))
-                    separator = 255*np.ones(shape=(1, frame_queue.width),
-                                            dtype=np.uint8)
                     sparse_frames = np.vstack((frame, separator,
                                                sparse, separator,
                                                sparse_filtered, separator,
@@ -102,10 +95,47 @@ def main(args):
                                                    folder_name=args.custom_dir,
                                                    scale=400)
 
+                    # TODO: Finish pipeline before tweaking stages further.
+                    # Below are unused, but potentially viable processing
+                    # stages. Don't touch these until you have the structure in
+                    # place to properly evaluate the tweaks you're making!!!!
+
+                    # BACKPROJECTION OR SOMETHING OR OTHER
+                    # _, mask = cv2.threshold(sparse_filtered,
+                    #                         thresh=0,
+                    #                         maxval=255,
+                    #                         type=(cv2.THRESH_BINARY+
+                    #                               cv2.THRESH_OTSU))
+                    # mask[mask == 255] = 1
+                    # sparse_thresh = np.multiply(sparse_uint8, mask)
+
+                    # GRAYSCALE MORPHOLOGICAL OPERATIONS
+                    # for i in range(5):
+                    #     sparse_opened = img.grey_opening(sparse_filtered,
+                    #                                      size=(2, 2))
+                    # sparse_eroded = img.grey_erosion(sparse_opened,
+                    #                                  size=(1, 1))
+                    #
+                    # sparse_opened = \
+                    #     img.grey_opening(sparse_thr, size=(2, 2)) \
+                    #     .astype(sparse_thr.dtype)
+                    # sparse_closed = \
+                    #    img.grey_closing(sparse_opened, size=(2, 2)) \
+                    #     .astype(sparse_thr.dtype)
+
             load_index += (1 + frame_queue.delay)
             if frame_queue.frames_read % 50 == 0:
                 print("{0}/{1} frames processed."
                       .format(frame_queue.frames_read, total_frames))
+
+        # Calculate error for total bird counts
+        ground_truth = np.c_[ground_truth,
+                             bird_count.reshape(-1, 1).astype(np.int)]
+        error = ground_truth[:, 1] - ground_truth[:, 6]
+        error_less = sum(error[error > 0])
+        error_more = -1* sum(error[error < 0])
+
+    breakpoint()
 
 
 if __name__ == "__main__":
@@ -121,7 +151,7 @@ if __name__ == "__main__":
                         nargs=2,
                         type=int,
                         metavar=('START_FRAME', 'TOTAL_FRAMES'),
-                        default=([16200, 200])
+                        default=([16150, 300])
                         )
     parser.add_argument("-d",
                         "--video_dir",
@@ -136,7 +166,7 @@ if __name__ == "__main__":
     parser.add_argument("-c",
                         "--custom_dir",
                         help="Custom directory for extracted frame files",
-                        default="Argument testing"
+                        default="Test Folder"
                         )
     parser.add_argument("-p",
                         "--crop",
