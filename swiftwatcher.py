699f4415e41b00050f1b388f58eaae94776a52da
joshuacwnewton <joshuacwnewton@gmail.com>
2019-07-21 08:01:23 -0700

    Fix bug with index for full-video test.

--- swiftwatcher.py
+++ swiftwatcher.py
@@ -161,7 +161,7 @@ if __name__ == "__main__":
                         nargs=2,
                         type=int,
                         metavar=('START_INDEX', 'END_INDEX'),
-                        default=([0, 108048])
+                        default=([0, 108047])
                         )
     parser.add_argument("-c",
                         "--custom_dir",
