26d0e18b566b53b0be6f36923a29a27d427b952c
joshuacwnewton <joshuacwnewton@gmail.com>
2019-08-02 21:45:06 -0700

    Testing improved threshold method on June 13th video.

--- swiftwatcher.py
+++ swiftwatcher.py
@@ -184,7 +184,7 @@ if __name__ == "__main__":
         parser.add_argument("-f",
                             "--filename",
                             help="Name of video file",
-                            default="ch04_20170518205849.mp4"
+                            default="NPD 460 CHSW 2019 June 13.mp4"
                             # ch04_20170518205849.mp4
                             # NPD 460 CHSW 2019 June 13.mp4
                             # NPD 541 CHSW 2019 June 14.mp4
@@ -192,12 +192,12 @@ if __name__ == "__main__":
         parser.add_argument("-t",
                             "--timestamp",
                             help="Specified starting timestamp for video",
-                            default="2017-05-18 00:00:00.000000"
+                            default="2019-06-13 00:00:00.000000"
                             )
         parser.add_argument("-n",
                             "--chimney",
                             help="Bottom corners which define chimney edge",
-                            default=[(748, 691), (920, 683)]
+                            default=[(810, 435), (1150, 435)]
                             # [(748, 691), (920, 683)]  <- ch04_20170518
                             # [(810, 435), (1150, 435)] <- june 13
                             # [(798, 449), (1164, 423)] <- june 14
@@ -213,12 +213,12 @@ if __name__ == "__main__":
                             nargs=2,
                             type=int,
                             metavar=('START_INDEX', 'END_INDEX'),
-                            default=([7200, 16200])
+                            default=([0, -1])
                             )
         parser.add_argument("-c",
                             "--custom_dir",
                             help="Custom directory for saving various things",
-                            default="/tests/2019-08-02_1-3x3-2x/"
+                            default="tests/2019-08-02_full-video/"
                             )
         parser.add_argument("-v",
                             "--visual",
--- swiftwatcher/data_analysis.py
+++ swiftwatcher/data_analysis.py
@@ -147,7 +147,7 @@ def generate_comparison(df_prediction, df_groundtruth):
 
         rows_to_drop = []
         for (i1, row1), (i2, row2) in pairwise(df_comparison.iterrows()):
-            if (type(i1) is list) and (type(i2) is list):
+            if (type(i1) is tuple) and (type(i2) is tuple):
                 i1 = i1[1]
                 i2 = i2[1]
 
@@ -176,7 +176,7 @@ def generate_comparison(df_prediction, df_groundtruth):
                     if np.array_equal(row2.values, [0, 0, 0]):
                         rows_to_drop.append(i2)
 
-        df_comparison_rm = df_comparison.drop(index=rows_to_drop)
+        df_comparison_rm = df_comparison.drop(level=1, index=rows_to_drop)
 
         assert (np.sum(df_groundtruth["ENTERGT"]) ==
                 np.sum(df_comparison_rm["ENTERGT"]))
