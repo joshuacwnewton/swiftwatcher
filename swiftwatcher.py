d620d757fb2cec6d897c1712070f26044881c17b
joshuacwnewton <joshuacwnewton@gmail.com>
2019-08-05 14:10:49 -0700

    Update TODOs for merge.

--- swiftwatcher.py
+++ swiftwatcher.py
@@ -4,9 +4,6 @@
 # -TODO: Get both ch04 and 13/14 to use same GT processing
 # -TODO: Automated batch processing (ch04, june 13, june 14 in one go)
 #
-# -TODO: Test without hard thresholding.
-# -TODO: Test with larger dilation for edge-based OTSU
-#
 # -TODO: Analyse all videos for shared sources of error
 # -TODO: Matching/feature: Area of segment (Avg?)
 # -TODO: Matching/feature: Diagonal length of segment (Avg?)
