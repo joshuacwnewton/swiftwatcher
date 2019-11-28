f7f113459c3cbc0ecc3837ac7b96c1e428e91487
Joshua Newton <joshuacwnewton@gmail.com>
2019-11-27 19:29:56 -0800

    Move swift_counting_algorithm() from video_processing.py to __main__.py

--- swiftwatcher/__main__.py
+++ swiftwatcher/__main__.py
@@ -7,6 +7,11 @@ from pathlib import Path
 import tkinter as tk
 from tkinter import filedialog
 
+# Import for swift_counting_algorithm
+import pandas as pd
+import cv2
+import sys
+
 
 def load_configs():
     """Load config files for videos in video directory, or create+save config
@@ -151,6 +156,101 @@ def load_configs():
     return configs
 
 
+def swift_counting_algorithm(config):
+    """Full algorithm which uses FrameQueue methods to process an entire video
+    from start to finish."""
+
+    def create_dataframe(passed_list):
+        """Convert list of events to pandas dataframe."""
+        dataframe = pd.DataFrame(passed_list,
+                                 columns=list(passed_list[0].keys())
+                                 ).astype('object')
+        dataframe["TMSTAMP"] = pd.to_datetime(dataframe["TMSTAMP"])
+        dataframe["TMSTAMP"] = dataframe["TMSTAMP"].dt.round('us')
+        dataframe.set_index(["TMSTAMP", "FRM_NUM"], inplace=True)
+
+        return dataframe
+
+    print("[*] Now processing {}.".format(config["name"]))
+    # print("[-]     Status updates will be given every 100 frames.")
+
+    fq = vid.FrameQueue(config)
+    while fq.frames_processed < fq.src_framecount:
+        success = False
+
+        # Store state variables in case video processing glitch occurs
+        # (e.g. due to poorly encoded video)
+        pos = fq.stream.get(cv2.CAP_PROP_POS_FRAMES)
+        read = fq.frames_read
+        proc = fq.frames_processed
+
+        try:
+            # Load frames until queue is filled
+            if fq.frames_read < (fq.queue_size - 1):
+                success = fq.load_frame()
+                fq.preprocess_frame()
+                # fq.segment_frame() (not needed until queue is filled)
+                # fq.match_segments() (not needed until queue is filled)
+                # fq.analyse_matches() (not needed until queue is filled)
+
+            # Process queue full of frames
+            elif (fq.queue_size - 1) <= fq.frames_read < fq.src_framecount:
+                success = fq.load_frame()
+                fq.preprocess_frame()
+                fq.segment_frame()
+                fq.match_segments()
+                fq.analyse_matches()
+
+            # Load blank frames until queue is empty
+            elif fq.frames_read == fq.src_framecount:
+                success = fq.load_frame(blank=True)
+                # fq.preprocess_frame() (not needed for blank frame)
+                fq.segment_frame()
+                fq.match_segments()
+                fq.analyse_matches()
+
+        except Exception as e:
+            # TODO: Print statements here should be replaced with logging
+            # Previous: print("[!] Error has occurred, see: '{}'.".format(e))
+            # I'm not satisfied with how unexpected errors are handled
+            fq.failcount += 1
+
+            # Increment state variables to ensure algorithm doesn't get stuck
+            if fq.stream.get(cv2.CAP_PROP_POS_FRAMES) == pos:
+                fq.stream.grab()
+            if fq.frames_read == read:
+                fq.frames_read += 1
+            if fq.frames_processed == proc:
+                fq.frames_processed += 1
+
+        if success:
+            fq.failcount = 0
+        else:
+            fq.failcount += 1
+
+        # Break if too many sequential errors
+        if fq.failcount >= 10:
+            # TODO: Print statements here should be replaced with logging
+            # Previous: print("[!] Too many sequential errors have occurred. "
+            #                 "Halting algorithm...")
+            # I'm not satisfied with how unexpected errors are handled
+            fq.frames_processed = fq.src_framecount + 1
+
+        # Status updates
+        if fq.frames_processed % 25 is 0 and fq.frames_processed is not 0:
+            sys.stdout.write("\r[-]     {0}/{1} frames processed.".format(
+                fq.frames_processed, fq.src_framecount))
+            sys.stdout.flush()
+
+    if fq.event_list:
+        df_eventinfo = create_dataframe(fq.event_list)
+    else:
+        df_eventinfo = []
+    print("")
+
+    return df_eventinfo
+
+
 def main():
     """Execute each of the core functions of the swift-counting algorithm."""
 
@@ -158,7 +258,7 @@ def main():
 
     for config in configs:
         if len(config["corners"]) == 2:
-            events = vid.swift_counting_algorithm(config)
+            events = swift_counting_algorithm(config)
 
             if len(events) > 0:
                 features = data.generate_feature_vectors(events)
