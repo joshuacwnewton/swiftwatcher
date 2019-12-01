fa64e4667ec6d8373503d01375c3ca12bc0759fb
Joshua Newton <joshuacwnewton@gmail.com>
2019-12-01 14:44:17 -0800

    Skeleton file structure for research experimentation functions.

---
+++ research/experimentation.py
@@ -0,0 +1,6 @@
+"""
+    Separate module containing versions of the swift counting algorithm
+    with additional functionality for experimentation (e.g. loading
+    frames from files, and visualizing frames at intermediate stages
+    of the algorithm).
+"""
---
+++ research/visualization.py
@@ -0,0 +1,4 @@
+"""
+    Separate module containing functionality to visualize various
+    aspects of the swift counting algorithm.
+"""
