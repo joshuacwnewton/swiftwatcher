d51365ee7dd22ba21025aa5074d03c181a17f3f6
Joshua Newton <joshuacwnewton@gmail.com>
2019-12-01 14:44:34 -0800

    Skeleton function structure for composite algorithms.

--- swiftwatcher_refactor/image_processing/composite_algorithms.py
+++ swiftwatcher_refactor/image_processing/composite_algorithms.py
@@ -2,3 +2,15 @@
     Contains multi-stage algorithms which are a composite of smaller,
     individual image processing algorithms.
 """
+
+
+def swift_counting_algorithm():
+    return None
+
+
+def preprocess_frame():
+    return None
+
+
+def segment_frame():
+    return None
