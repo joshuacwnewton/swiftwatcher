65e1e5c76a65871a04cf99e69dd1310dcb4cc34f
Joshua Newton <joshuacwnewton@gmail.com>
2019-12-02 09:15:05 -0800

    Wrote entry point scripts for calling params, algorithm, data analysis.

---
+++ research/scripts/main_test_script.py
@@ -0,0 +1,33 @@
+"""
+    Equivalent to __main__.py in swiftwatcher, but provides additional
+    functionality not found in base project. See experimentation.py
+"""
+
+import swiftwatcher_refactor.io.ui as ui
+import swiftwatcher_refactor.io.video_io as vio
+import swiftwatcher_refactor.io.data_io as dio
+import swiftwatcher_refactor.image_processing.image_filtering as img
+import swiftwatcher_refactor.data_analysis.event_classification as ec
+import research.experimentation as exp
+
+# Generate necessary input parameters for swift counting algorithm
+filepath, start, end = ui.parse_filepath_and_framerange()
+vio.validate_video_filepath(filepath)
+vio.validate_frame_range(filepath.parent/filepath.stem/"frames",
+                         start, end)
+corners = exp.get_corners_from_file(filepath.parent/filepath.stem/"frames")
+crop_region, roi_mask, resize_dim = img.generate_regions(filepath, corners)
+
+# Apply algorithm to detect events and then classify them
+events = exp.swift_counting_algorithm(filepath,
+                                      crop_region, resize_dim, roi_mask,
+                                      start, end)
+label_dataframes = ec.classify_events(events)
+
+# Save results to unique test directory
+test_dir = exp.generate_test_dir(filepath.parent/filepath.stem/"tests")
+dio.dataframe_to_csv(test_dir, events)
+for label_dataframe in label_dataframes:
+    dio.dataframe_to_csv(test_dir, label_dataframe)
+
+exit(0)
--- swiftwatcher_refactor/__main__.py
+++ swiftwatcher_refactor/__main__.py
@@ -1,16 +1,18 @@
 import swiftwatcher_refactor.io.ui as ui
 import swiftwatcher_refactor.io.data_io as dio
-import swiftwatcher_refactor.io.video_io as vio
-import swiftwatcher_refactor.image_processing.composite_algorithms as alg
+import swiftwatcher_refactor.image_processing.image_filtering as img
+import swiftwatcher_refactor.image_processing.primary_algorithm as alg
 import swiftwatcher_refactor.data_analysis.event_classification as ec
 
 
 video_filepaths = ui.select_video_files()
-video_attr_list = vio.get_video_attributes(video_filepaths)
 
-for video_attr in video_attr_list:
-    events = alg.swift_counting_algorithm(filepath=video_attr["filepath"],
-                                          corners=video_attr["corners"])
+for video_filepath in video_filepaths:
+    corners = ui.select_chimney_corners(video_filepath)
+    crop_region, roi_mask, resize_dim = img.generate_regions(video_filepath,
+                                                             corners)
+    events = alg.swift_counting_algorithm(video_filepath,
+                                          crop_region, roi_mask, resize_dim)
     label_dataframes = ec.classify_events(events)
     for label_dataframe in label_dataframes:
         dio.dataframe_to_csv(label_dataframe)
