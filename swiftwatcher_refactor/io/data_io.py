fbe476cafe81942996bf2471e167c32819a935ea
Joshua Newton <joshuacwnewton@gmail.com>
2019-12-07 07:35:46 -0800

    Wrote function to output dataframe to csv as-is.

--- swiftwatcher_refactor/io/data_io.py
+++ swiftwatcher_refactor/io/data_io.py
@@ -2,3 +2,12 @@
     Contains functionality for I/O of tabular data. (e.g. pandas
     DataFrames, csv-formatted data, etc.)
 """
+
+from pathlib import Path
+
+
+def dataframe_to_csv(dataframe, output_filepath):
+    if not output_filepath.parent.exists():
+        Path.mkdir(output_filepath.parent, parents=True)
+
+    dataframe.to_csv(str(output_filepath))
