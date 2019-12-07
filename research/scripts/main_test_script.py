b3ebe1f2e1c0515d2f2caedeebad52c2cc275b77
Joshua Newton <joshuacwnewton@gmail.com>
2019-12-07 08:40:52 -0800

    Updated main_test_script.py to match new location for experimentation funcs.

--- research/scripts/main_test_script.py
+++ research/scripts/main_test_script.py
@@ -1,6 +1,6 @@
 """
     Equivalent to __main__.py in swiftwatcher, but provides additional
-    functionality not found in base project. See experimentation.py
+    functionality not found in base project.
 """
 
 import swiftwatcher_refactor.io.ui as ui
@@ -8,7 +8,7 @@ import swiftwatcher_refactor.io.video_io as vio
 import swiftwatcher_refactor.io.data_io as dio
 import swiftwatcher_refactor.image_processing.image_filtering as img
 import swiftwatcher_refactor.data_analysis.event_classification as ec
-import research.experimentation as exp
+import swiftwatcher_refactor.image_processing.primary_algorithm as alg
 
 # Generate necessary input parameters for swift counting algorithm
 filepath, start, end = ui.parse_filepath_and_framerange()
@@ -18,21 +18,23 @@ properties = vio.get_video_properties(filepath)
 frame_dir = filepath.parent/filepath.stem/"frames"
 vio.validate_frame_range(frame_dir, start, end)
 
-corners = exp.get_corners_from_file(frame_dir)
+corners = vio.get_corners_from_file(frame_dir)
 crop_region, roi_mask, resize_dim = img.generate_regions(filepath, corners)
 
 # Apply algorithm to detect events, then classify detected events
-events = exp.swift_counting_algorithm(frame_dir,
+events = alg.swift_counting_algorithm(frame_dir,
                                       crop_region, resize_dim, roi_mask,
                                       properties["fps"], start, end)
 df_events = ec.convert_events_to_dataframe(events, ["parent_frame_number",
                                                     "parent_timestamp",
                                                     "centroid"])
-label_dataframes = ec.classify_events(df_events)
+df_labels = ec.classify_events(df_events)
 
 # Save results to unique test directory
-# test_dir = exp.generate_test_dir(filepath.parent/filepath.stem/"tests")
-# dio.dataframe_to_csv(test_dir, events)
+test_dir = vio.generate_test_dir(filepath.parent/filepath.stem/"tests")
+dio.dataframe_to_csv(df_events, test_dir/"df_events.csv")
+dio.dataframe_to_csv(df_labels, test_dir/"df_labels.csv")
+
 # for label_dataframe in label_dataframes:
 #     dio.dataframe_to_csv(test_dir, label_dataframe)
 
