39565e4ad3d0f19a52aebb1e834a4fded9c04202
Joshua Newton <joshuacwnewton@gmail.com>
2019-12-07 08:40:23 -0800

    Moved experimentation funcs (test_dir, corners_from_file) to video_io.py.

--- swiftwatcher_refactor/io/video_io.py
+++ swiftwatcher_refactor/io/video_io.py
@@ -6,6 +6,9 @@ import sys
 from pathlib import Path
 from glob import glob
 
+import json
+from datetime import date
+
 import pandas as pd
 import numpy as np
 import cv2
@@ -288,3 +291,48 @@ class VideoReader(cv2.VideoCapture):
 
     def ms_to_ts(self, ms):
         return pd.Timestamp("00:00:00.000") + pd.Timedelta(ms, 'ms')
+
+
+###############################################################################
+#               RESEARCH EXPERIMENTATION FUNCTIONS BEGIN HERE                 #
+###############################################################################
+
+
+def get_corners_from_file(parent_directory):
+    """Non-GUI alternative to get_video_attributes for time saving."""
+
+    with open(str(parent_directory / "attributes.json")) as json_file:
+        video_attributes = json.load(json_file)
+
+        # Convert from string to individual integer values
+        video_attributes["corners"] \
+            = [(int(video_attributes["corners"][0][0]),
+                int(video_attributes["corners"][0][1])),
+               (int(video_attributes["corners"][1][0]),
+                int(video_attributes["corners"][1][1]))]
+
+    return video_attributes["corners"]
+
+
+def generate_test_dir(parent_dir):
+    """Generate test directory based on the following scheme:
+        parent_dir/<today's date>/<ID of last test + 1>
+
+    If no test has been run today, set ID to 1."""
+
+    # Set base testing directory to today's date
+    date_dir = parent_dir / str(date.today())
+
+    if not date_dir.exists():
+        # Date directory doesnt exist, so must be first test run today
+        test_dir = date_dir / "1"
+
+    else:
+        # Fetch names of all subdirectories in date_dir, then get the max
+        last_test_id = max([int(path.stem) for path in
+            [Path(path_str) for path_str in glob(str(date_dir / "*/"))]])
+
+        # Set test directory to last test incremented by one
+        test_dir = date_dir / str(last_test_id + 1)
+
+    return test_dir
